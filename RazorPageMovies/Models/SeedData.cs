d982aa1e1b4b4e5d9959606b6db673edf0d563e4
jcristian12 <98921865+jcristian12@users.noreply.github.com>
2024-02-24 13:19:50 -0600

    Agregar archivos de proyecto.

---
+++ RazorPageMovies/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPageMovies.Data;
+
+namespace RazorPageMovies.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPageMoviesContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPageMoviesContext>>())) 
+            {
+                if(context == null) { 
+                    throw new ArgumentNullException("Null RazorPagesMovies");
+                }
+
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+
+                context.Movie.AddRange(
+                    
+                    new Movie 
+                    { 
+                        Title = "Pelicula 1",
+                        ReleaseDate = DateTime.Now,
+                        Genre = "romantica",
+                        Price = 7.99M,
+                        Rating = "g"
+                    
+                    },
+                     new Movie
+                     {
+                         Title = "Pelicula 2",
+                         ReleaseDate = DateTime.Parse("1989-02-10"),
+                         Genre = "romantica",
+                         Price = 7.99M,
+                         Rating = "r"
+                     },
+                      new Movie
+                      {
+                          Title = "Pelicula 3",
+                          ReleaseDate = DateTime.Parse("2000-04-10"),
+                          Genre = "romantica",
+                          Price = 7.99M,
+                          Rating = "a"
+                      }
+
+                    );
+
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
---
+++ RazorPageMovies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovies.Data;
+using RazorPageMovies.Models;
+
+namespace RazorPageMovies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovies.Data.RazorPageMoviesContext _context;
+
+        public IndexModel(RazorPageMovies.Data.RazorPageMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; } = default!;
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Generos { get; set; }
+        [BindProperty(SupportsGet = true)]
+
+        public string? MovieGenero { get; set; }
+
+
+        public async Task OnGetAsync()
+        {
+            //Movie = await _context.Movie.ToListAsync();
+
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
+            var movies = from m in _context.Movie select m;
+
+            if (!string.IsNullOrEmpty(SearchString)) 
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenero))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenero);
+            }
+
+            if (_context.Movie != null) 
+            {
+                Generos = new SelectList(await genreQuery.Distinct().ToListAsync());
+                Movie = await movies.ToListAsync();
+            }
+
+            
+
+        }
+    }
+}
